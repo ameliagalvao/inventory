29b6daeec5ed91b2b6c83fc12abdba05d98c4a29
amelia <ameliamgalvao@gmail.com>
2022-09-22 07:33:55 -0300

    Created

---
+++ app/src/main/java/com/miigubymia/inventory/artisans/RVArtisansActivity.kt
@@ -0,0 +1,12 @@
+package com.miigubymia.inventory.artisans
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.miigubymia.inventory.R
+
+class RVArtisansActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rvartisans)
+    }
+}
