ca82de82cbb7e5b897036f0395bd3f94dd5a835e
amelia <ameliamgalvao@gmail.com>
2022-09-22 08:30:56 -0300

    Database class

---
+++ app/src/main/java/com/miigubymia/inventory/dataBase/InventoryDatabase.kt
@@ -0,0 +1,30 @@
+package com.miigubymia.inventory.dataBase
+
+import android.content.Context
+import androidx.room.RoomDatabase
+import androidx.room.Database
+import androidx.room.Room
+
+@Database(entities = [Artisan::class], version = 1)
+abstract class InventoryDatabase: RoomDatabase() {
+
+    abstract fun getRoomDao():RoomDAO
+
+    companion object{
+        // o companion permite que a gente acesse essa variável de qualquer lugar do app
+        // o volatile deixa a instância criada a partir dessa thread visível para todas as outras threads.
+        @Volatile
+        private var INSTANCE: InventoryDatabase? = null
+
+        fun getDatabase(context: Context):InventoryDatabase{
+            // só permite a criação de uma instância por vez.
+            return INSTANCE ?: synchronized(this){
+                val instance = Room.databaseBuilder(context.applicationContext,
+                InventoryDatabase::class.java,"inventory_database").build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+
+}
