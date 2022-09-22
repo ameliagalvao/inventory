3ecdf3eef621258841daff1dd7c07476df7faacd
amelia <ameliamgalvao@gmail.com>
2022-09-22 13:52:27 -0300

    artisan adapter and recycler view

---
+++ app/src/main/java/com/miigubymia/inventory/artisans/ArtisanAdapter.kt
@@ -0,0 +1,48 @@
+package com.miigubymia.inventory.artisans
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import com.miigubymia.inventory.R
+import com.miigubymia.inventory.dataBase.Artisan
+
+class ArtisanAdapter: RecyclerView.Adapter<ArtisanAdapter.ArtisanViewHolder>() {
+
+    var artisans: List<Artisan> = ArrayList()
+
+    class ArtisanViewHolder (itemView: View):RecyclerView.ViewHolder(itemView){
+        val textViewArtisanName:TextView = itemView.findViewById(R.id.tvArtisanName)
+        val textViewArtisanPix:TextView = itemView.findViewById(R.id.tvArtisanPix)
+        val textViewArtisanSkills:TextView = itemView.findViewById(R.id.tvArtisanSkills)
+        val imageViewArtisanPhoto:ImageView = itemView.findViewById(R.id.ivArtisanPhoto)
+        val cardView:CardView = itemView.findViewById(R.id.artisanCardView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArtisanViewHolder {
+        val view:View = LayoutInflater.from(parent.context)
+            .inflate(R.layout.artisan_card,parent,false)
+        return ArtisanViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ArtisanViewHolder, position: Int) {
+        var currentArtisan : Artisan = artisans[position]
+
+        holder.textViewArtisanName.text = currentArtisan.artisanName
+        holder.textViewArtisanPix.text = currentArtisan.artisanPix
+        holder.textViewArtisanSkills.text = currentArtisan.artisanSkills
+        holder.imageViewArtisanPhoto.setImageResource(R.drawable.p1)
+    }
+
+    override fun getItemCount(): Int {
+        return artisans.size
+    }
+
+    fun setArtisan(refreshArtisan: List<Artisan>){
+        this.artisans = refreshArtisan
+        notifyDataSetChanged()
+    }
+}
--- app/src/main/java/com/miigubymia/inventory/artisans/RVArtisansActivity.kt
+++ app/src/main/java/com/miigubymia/inventory/artisans/RVArtisansActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.miigubymia.inventory.R
 import com.miigubymia.inventory.dataBase.ArtisanViewModel
 import com.miigubymia.inventory.dataBase.ArtisanViewModelFactory
@@ -22,10 +24,15 @@ class RVArtisansActivity : AppCompatActivity() {
         setContentView(view)
 
         val viewModelFactory = ArtisanViewModelFactory((application as InventoryApplication).repository)
+        val recyclerView:RecyclerView = binding.artisiansListRecyclerView
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        val artisanAdapter = ArtisanAdapter()
+        recyclerView.adapter = artisanAdapter
+
 
         artisanViewModel = ViewModelProvider(this, viewModelFactory).get(ArtisanViewModel::class.java)
-        artisanViewModel.allArtisans.observe(this,Observer{
-
+        artisanViewModel.allArtisans.observe(this,Observer{artisans ->
+            artisanAdapter.setArtisan(artisans)
         })
 
     }
