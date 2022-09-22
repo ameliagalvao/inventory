28bdf02a02f353b1ae85e28618af43f113793ff2
amelia <ameliamgalvao@gmail.com>
2022-09-22 10:49:48 -0300

    create application and database data example

--- app/src/main/java/com/miigubymia/inventory/dataBase/InventoryApplication.kt
+++ app/src/main/java/com/miigubymia/inventory/dataBase/InventoryApplication.kt
@@ -1,10 +1,12 @@
 package com.miigubymia.inventory.dataBase
 
 import android.app.Application
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
 
 class InventoryApplication: Application() {
-
-    val database by lazy {InventoryDatabase.getDatabase(this)}
+    val applicationScope = CoroutineScope(SupervisorJob())
+    val database by lazy {InventoryDatabase.getDatabase(this, applicationScope)}
     val repository by lazy { InventoryRepository(database.getInventoryDao()) }
 
 }
--- app/src/main/java/com/miigubymia/inventory/dataBase/InventoryDatabase.kt
+++ app/src/main/java/com/miigubymia/inventory/dataBase/InventoryDatabase.kt
@@ -4,6 +4,9 @@ import android.content.Context
 import androidx.room.RoomDatabase
 import androidx.room.Database
 import androidx.room.Room
+import androidx.sqlite.db.SupportSQLiteDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 @Database(entities = [Artisan::class], version = 1)
 abstract class InventoryDatabase: RoomDatabase() {
@@ -16,15 +19,32 @@ abstract class InventoryDatabase: RoomDatabase() {
         @Volatile
         private var INSTANCE: InventoryDatabase? = null
 
-        fun getDatabase(context: Context):InventoryDatabase{
+        fun getDatabase(context: Context, scope: CoroutineScope):InventoryDatabase{
             // só permite a criação de uma instância por vez.
             return INSTANCE ?: synchronized(this){
                 val instance = Room.databaseBuilder(context.applicationContext,
-                InventoryDatabase::class.java,"inventory_database").build()
+                InventoryDatabase::class.java,"inventory_database")
+                    .addCallback(InventoryDatabaseCallback(scope))
+                    .build()
                 INSTANCE = instance
                 instance
             }
         }
     }
 
+    private class InventoryDatabaseCallback(private val scope: CoroutineScope): RoomDatabase.Callback(){
+        override fun onCreate(db: SupportSQLiteDatabase) {
+            super.onCreate(db)
+
+            INSTANCE?.let { database ->
+                scope.launch {
+                    val inventoryDAO = database.getInventoryDao()
+                    inventoryDAO.insertArtisan(Artisan("Cicrana da Silva", "Telefone 81988868584", "81988868584", "Amigurumi, Crochê"))
+                    inventoryDAO.insertArtisan(Artisan("Fulano dos Santos", "CPF 89578435972", "81988875652", "Macramê"))
+                    inventoryDAO.insertArtisan(Artisan("Beltrana das Graças", "Email beltrana@provedor.com", "81994858264", "Costura, CrochÊ"))
+                }
+            }
+
+        }
+    }
 }
