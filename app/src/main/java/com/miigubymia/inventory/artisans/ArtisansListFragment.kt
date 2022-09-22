f3cd453ea9604192854de222c19dfc0dfd9e13f4
amelia <ameliamgalvao@gmail.com>
2022-09-22 17:48:18 -0300

    Fragmento funcionando

--- app/src/main/java/com/miigubymia/inventory/artisans/ArtisansListFragment.kt
+++ app/src/main/java/com/miigubymia/inventory/artisans/ArtisansListFragment.kt
@@ -5,15 +5,70 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.miigubymia.inventory.R
+import com.miigubymia.inventory.dataBase.ArtisanViewModel
+import com.miigubymia.inventory.dataBase.ArtisanViewModelFactory
+import com.miigubymia.inventory.dataBase.InventoryApplication
+import androidx.lifecycle.Observer
+import androidx.appcompat.widget.SearchView
 
 class ArtisansListFragment : Fragment() {
+
+    lateinit var artisanViewModel: ArtisanViewModel
+    lateinit var artisanAdapter:ArtisanAdapter
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val viewModelFactory = ArtisanViewModelFactory((activity?.applicationContext as InventoryApplication).repository)
+        artisanViewModel = ViewModelProvider(this, viewModelFactory).get(ArtisanViewModel::class.java)
+        artisanViewModel.allArtisans.observe(viewLifecycleOwner,Observer{artisans ->
+            artisanAdapter.setArtisan(artisans)
+        })
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_artisans_list, container, false)
+
+        val recyclerView: RecyclerView = view.findViewById(R.id.rvArtisansListFragment)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        artisanAdapter = ArtisanAdapter()
+        recyclerView.adapter = artisanAdapter
+
+        //SearchView
+        val searchView = view.findViewById<SearchView>(R.id.searchBarArtisan)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                if(query != null){
+                    searchArtisansInDatabase(query)
+                }
+                return true
+            }
+
+            //vai ser chamada sempre que digitarmos um caracter no view
+            override fun onQueryTextChange(query: String?): Boolean {
+                if(query != null){
+                    searchArtisansInDatabase(query)
+                }
+                return true
+            }
+        })
+
         return view
     }
+
+    private fun searchArtisansInDatabase(query:String){
+        val searchQuery = "%$query%"
+        artisanViewModel.searchArtisan(searchQuery).observe(this) { list ->
+            list.let {
+                artisanAdapter.setArtisan(it)
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/miigubymia/inventory/artisans/RVArtisansActivity.kt
+++ app/src/main/java/com/miigubymia/inventory/artisans/RVArtisansActivity.kt
@@ -2,20 +2,12 @@ package com.miigubymia.inventory.artisans
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.miigubymia.inventory.dataBase.ArtisanViewModel
-import com.miigubymia.inventory.dataBase.ArtisanViewModelFactory
-import com.miigubymia.inventory.dataBase.InventoryApplication
 import com.miigubymia.inventory.databinding.ActivityRvartisansBinding
 
 class RVArtisansActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityRvartisansBinding
-    lateinit var artisanViewModel: ArtisanViewModel
-    lateinit var artisanAdapter:ArtisanAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,47 +15,5 @@ class RVArtisansActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        val viewModelFactory = ArtisanViewModelFactory((application as InventoryApplication).repository)
-        val recyclerView:RecyclerView = binding.artisiansListRecyclerView
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        artisanAdapter = ArtisanAdapter()
-        recyclerView.adapter = artisanAdapter
-
-
-        artisanViewModel = ViewModelProvider(this, viewModelFactory).get(ArtisanViewModel::class.java)
-
-        artisanViewModel.allArtisans.observe(this,Observer{artisans ->
-            artisanAdapter.setArtisan(artisans)
-        })
-
-        //SearchView
-        val searchView = binding.searchBarArtisan
-        searchView.setOnQueryTextListener(object : androidx.appcompat.widget.SearchView.OnQueryTextListener {
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                if(query != null){
-                    searchArtisansInDatabase(query)
-                }
-                return true
-            }
-
-            //vai ser chamada sempre que digitarmos um caracter no view
-            override fun onQueryTextChange(query: String?): Boolean {
-                if(query != null){
-                    searchArtisansInDatabase(query)
-                }
-                return true
-            }
-        })
-
     }
-
-        private fun searchArtisansInDatabase(query:String){
-            val searchQuery = "%$query%"
-            artisanViewModel.searchArtisan(searchQuery).observe(this) { list ->
-                list.let {
-                    artisanAdapter.setArtisan(it)
-                }
-            }
-        }
-
 }
