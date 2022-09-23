a0f1ceeb021502443d1b21c0c7b8f79317890930
amelia <ameliamgalvao@gmail.com>
2022-09-23 11:28:12 -0300

    fragmentos criados

---
+++ app/src/main/java/com/miigubymia/inventory/artisans/SingleArtisanActivity.kt
@@ -0,0 +1,12 @@
+package com.miigubymia.inventory.artisans
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.miigubymia.inventory.R
+
+class SingleArtisanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_single_artisan)
+    }
+}
