3cbc2ea50b56a2cde427cd582eeea34fd1126375
amelia <ameliamgalvao@gmail.com>
2022-09-23 20:40:51 -0300

    voltando para a activity

--- app/src/main/java/com/miigubymia/inventory/artisans/SinglePageArtisanFragment.kt
+++ app/src/main/java/com/miigubymia/inventory/artisans/SinglePageArtisanFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import com.miigubymia.inventory.R
 import com.miigubymia.inventory.dataBase.ArtisanViewModel
@@ -35,14 +37,23 @@ class SinglePageArtisanFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_single_page_artisan, container, false)
         var currentArtisan = activity?.intent?.getSerializableExtra("clickedArtisan") as Artisan
+
         val nameTextView = view.findViewById<TextView>(R.id.tvSingleArtisanName)
         val pixTextView = view.findViewById<TextView>(R.id.tvSinglePixToFill)
         val phoneTextView = view.findViewById<TextView>(R.id.tvSinglePhoneToFill)
         val skillsTextView = view.findViewById<TextView>(R.id.tvSingleSkillsToFill)
+        val deletebtn = view.findViewById<Button>(R.id.btnSingleDelete)
+
         nameTextView.text = currentArtisan.artisanName
         pixTextView.text = currentArtisan.artisanPix
         phoneTextView.text = currentArtisan.artisanPhone
         skillsTextView.text = currentArtisan.artisanSkills
+
+        deletebtn.setOnClickListener {
+            requireActivity().onBackPressed()
+            Toast.makeText(context, "Artesão Excluído", Toast.LENGTH_SHORT).show()
+        }
+
         return view
     }
 
