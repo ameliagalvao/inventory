63e4d0226b23a91027a425fe105f186e9305225d
amelia <ameliamgalvao@gmail.com>
2022-09-23 15:55:21 -0300

    fragmento single funcionando

--- app/src/main/java/com/miigubymia/inventory/artisans/SingleArtisanActivity.kt
+++ app/src/main/java/com/miigubymia/inventory/artisans/SingleArtisanActivity.kt
@@ -3,10 +3,17 @@ package com.miigubymia.inventory.artisans
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.miigubymia.inventory.R
+import com.miigubymia.inventory.databinding.ActivitySingleArtisanBinding
 
 class SingleArtisanActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySingleArtisanBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_single_artisan)
+        binding = ActivitySingleArtisanBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
     }
 }
--- app/src/main/java/com/miigubymia/inventory/artisans/SinglePageArtisanFragment.kt
+++ app/src/main/java/com/miigubymia/inventory/artisans/SinglePageArtisanFragment.kt
@@ -6,15 +6,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.miigubymia.inventory.R
+import com.miigubymia.inventory.databinding.FragmentSinglePageArtisanBinding
 
 class SinglePageArtisanFragment : Fragment() {
+
+    private var _binding:FragmentSinglePageArtisanBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_single_page_artisan, container, false)
+        _binding = FragmentSinglePageArtisanBinding.inflate(inflater, container, false)
+        val view = inflater.inflate(R.layout.fragment_single_page_artisan, container, false)
+
+
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
