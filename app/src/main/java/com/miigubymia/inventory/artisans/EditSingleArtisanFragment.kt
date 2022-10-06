eb22c77ff8ccd41c4f11e2534d6b395fbf93ff01
amelia <ameliamgalvao@gmail.com>
2022-10-06 15:36:30 -0300

    checkPixEdit

--- app/src/main/java/com/miigubymia/inventory/artisans/EditSingleArtisanFragment.kt
+++ app/src/main/java/com/miigubymia/inventory/artisans/EditSingleArtisanFragment.kt
@@ -4,14 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
+import android.widget.*
 import androidx.fragment.app.DialogFragment
 import com.miigubymia.inventory.R
 import com.miigubymia.inventory.dataBase.Artisan
 
 class EditSingleArtisanFragment : DialogFragment() {
+
+    lateinit var radioGroupEdit:RadioGroup
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,11 +20,23 @@ class EditSingleArtisanFragment : DialogFragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_edit_single_artisan, container, false)
 
+        var currentArtisan = activity?.intent?.getSerializableExtra("clickedArtisan") as Artisan
+
         val cancelbtn = view.findViewById<Button>(R.id.btnCancelEdit)
         val confirmbtn = view.findViewById<Button>(R.id.btnEditConfirm)
-        var currentArtisan = activity?.intent?.getSerializableExtra("clickedArtisan") as Artisan
+
         val editTextName = view.findViewById<EditText>(R.id.etNameEdit)
         editTextName.setHint(currentArtisan.artisanName)
+
+        val editTextPhone = view.findViewById<EditText>(R.id.etEditPhone)
+        editTextPhone.setHint(currentArtisan.artisanPhone)
+
+        val editTextPix = view.findViewById<EditText>(R.id.etEditArtisanPix)
+        val pixWithoutRadioBtn = currentArtisan.artisanPix.substringAfterLast(' ')
+        editTextPix.setHint(pixWithoutRadioBtn)
+
+        radioGroupEdit = view.findViewById(R.id.radioGroupEditPix)
+        checkPixOption(currentArtisan.artisanPix)
 
         cancelbtn.setOnClickListener {
             dialog!!.dismiss()
@@ -36,4 +48,14 @@ class EditSingleArtisanFragment : DialogFragment() {
 
         return view
     }
+
+    private fun checkPixOption(pix: String) {
+        when {
+            pix.contains("Telefone") -> radioGroupEdit.check(R.id.rbtnEditPhone)
+            pix.contains("E-mail") || pix.contains("Email") -> radioGroupEdit.check(R.id.rbtnEditEmail)
+            pix.contains("CPF") -> radioGroupEdit.check(R.id.rbtnEditCPF)
+            else -> false
+        }
+    }
+
 }
